964306dc7719e3ebe3bd2c37e8b430a8027199e2
Walker-237 <bryandongmo51@gmail.com>
2025-02-10 14:18:31 +0100

    Login form, Registration form, Admin dashboard and Staff Dashboard done

---
+++ src/main/java/com/mycompany/bicycle_system/Bicycle_System.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bicycle_system;
+import UI.*;
+
+/**
+ *
+ * @author Walker
+ */
+public class Bicycle_System {
+    class User{
+        public int id;      
+        public String name,email,password,status;
+        User(int id,String name,String email,String password,String status)
+        {
+            this.id = id;           
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.status = status;
+        }
+    }
+  
+    class Customer extends User{  
+        Customer(int id,String name,String email,String password,String status){
+            super(id,name,email,password,status);
+        }
+       void reserveBicycle(){}
+        void viewCatalogue(){}
+        void manageTransaction(){}
+        void manageCart(){}
+    }
+      
+    class StockManager extends User{  
+        StockManager(int id,String name,String email,String password,String status){
+            super(id,name,email,password,status);
+        }
+       void updateBicycleStatus(){}
+        void checkSellingStatistics(){}
+        void setPrice(){}
+        void setQuantity(){}
+    }
+          
+    class Admin extends User{  
+        Admin(int id,String name,String email,String password,String status){
+            super(id,name,email,password,status);
+        }
+       void manageUSerAccount(){}
+    }
+    
+    public static void main(String[] args) {
+        ManageBicyclePost w = new ManageBicyclePost();
+        w.setVisible(true);
+    }
+}
